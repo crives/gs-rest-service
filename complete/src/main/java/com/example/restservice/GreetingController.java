6a17c8b33b3af2353e2cc07b789df2771e19fd2a
Caroline Rives <car.rives@gmail.com>
2022-08-31 18:53:48 -0400

    New files for DB Environment Setup lab

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -2,6 +2,7 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+	@Autowired
+	private CounterRepository counterRepository;
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
@@ -16,4 +19,27 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+//	@GetMapping("/persistent_greeting")
+//	public Greeting persistentGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+//		Counter persistentCounter = counterRepository.findById(1L).orElseGet(() -> new Counter(0L));
+//		Long persistentCounterValue = persistentCounter.incrementAndGet();
+//		counterRepository.save(persistentCounter);
+//		return new Greeting(persistentCounterValue, String.format(template, name));
+//	}
+
+	@GetMapping("/set_count")
+	public Greeting setCount(@RequestParam(value = "value", defaultValue = "0") Long value) {
+		Counter newCounter = counterRepository.findById(1L).orElseGet(() -> new Counter(0L));
+		newCounter.setCounter(value);
+		counterRepository.save(newCounter);
+		return new Greeting(newCounter.getCounter(), "Count value set.");
+	}
+
+	@GetMapping("/get_count")
+	public Greeting getCount() {
+		Counter newCounter = counterRepository.findById(1L).orElseGet(() -> new Counter(0L));
+		return new Greeting(newCounter.getCounter(), "Count value get.");
+	}
+
 }
