69352a118c475cd6106632084854d054587b6d8d
Caroline Rives <car.rives@gmail.com>
2022-09-01 21:57:23 -0400

    fix app

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestServiceApplication {
 
     public static void main(String[] args) {
+        System.setProperty("datastax-java-driver.advanced.request-tracker.class", "RequestLogger");
+        System.setProperty("datastax-java-driver.advanced.request-tracker.logs.success.enabled", "true");
         SpringApplication.run(RestServiceApplication.class, args);
     }
-
 }
